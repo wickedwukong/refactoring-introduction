9ff846ebd43f5e4d8e15bef1ebc085872ee5a1c9
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-11-06 15:41:07 +0000

    First commit: adding the code to be refactored and and a test for it

---
+++ test/statement.test.js
@@ -0,0 +1,43 @@
+import { expect } from 'chai';
+import {statement} from '../src/statement.js'
+
+describe('statement', () => {
+    let playsJson =
+        {
+            "hamlet": {"name": "Hamlet", "type": "tragedy"},
+            "as-like": {"name": "As You Like It", "type": "comedy"},
+            "othello": {"name": "Othello", "type": "tragedy"}
+        }
+
+    let  invoicesJson =
+        [
+            {
+                "customer": "BigCo",
+                "performances": [
+                    {
+                        "playID": "hamlet",
+                        "audience": 55
+                    },
+                    {
+                        "playID": "as-like",
+                        "audience": 35
+                    },
+                    {
+                        "playID": "othello",
+                        "audience": 40
+                    }
+                ]
+            }
+        ]
+    it('should print a statement for multiple plays, single customer and multiple seats', () => {
+        let expected = "Statement for BigCo\n" +
+        "  Hamlet: $650.00 (55 seats)\n" +
+        "  As You Like It: $580.00 (35 seats)\n" +
+        "  Othello: $500.00 (40 seats)\n" +
+        "Amount owed is $1,730.00\n" +
+        "You earned 47 credits\n";
+
+        expect(statement(invoicesJson[0], playsJson)).to.equal(expected);
+    });
+
+});
