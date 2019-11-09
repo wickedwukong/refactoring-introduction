266d0efabb9771fed6b4e6d0e8eb0c9d575f154c
Xuemin Guan <Xuemin Guan>
2019-11-09 23:57:08 +0000

    Extract totalAmount by spliiting loop, extract function, slde statement, and inline variables

--- src/statement.js
+++ src/statement.js
@@ -51,15 +51,20 @@ function statement (invoice, plays) {
         return volumeCredits;
     }
 
-    let totalAmount = 0;
+    function totalAmount() {
+        let totalAmount = 0;
+        for (let perf of invoice.performances) {
+            totalAmount += amountFor(perf);
+        }
+        return totalAmount;
+    }
+
     let result = `Statement for ${invoice.customer}\n`;
     for (let perf of invoice.performances) {
-        // print line for this order
         result += `  ${playFor(perf).name}: ${(usd(amountFor(perf)))} (${perf.audience} seats)\n`;
-        totalAmount += amountFor(perf);
     }
-    
-    result += `Amount owed is ${usd(totalAmount)}\n`;
+
+    result += `Amount owed is ${usd(totalAmount())}\n`;
     result += `You earned ${(totalVolumeCredits())} credits\n`;
     return result;
 }
