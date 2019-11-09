73a336801962ac5685bc39a5666a549fc9f20f7d
Xuemin Guan <Xuemin Guan>
2019-11-09 17:43:45 +0000

    Replace Temp with Query to calculate play so that it does not get passed in and remove local variable play

--- src/statement.js
+++ src/statement.js
@@ -1,9 +1,13 @@
 export {statement}
 
 function statement (invoice, plays) {
-    function amountFor(play, aPerformance) {
+    function playFor(perf) {
+        return plays[perf.playID];
+    }
+
+    function amountFor(aPerformance) {
         let result = 0;
-        switch (play.type) {
+        switch (playFor(aPerformance).type) {
             case "tragedy":
                 result = 40000;
                 if (aPerformance.audience > 30) {
@@ -18,7 +22,7 @@ function statement (invoice, plays) {
                 result += 300 * aPerformance.audience;
                 break;
             default:
-                throw new Error(`unknown type: ${play.type}`);
+                throw new Error(`unknown type: ${playFor(aPerformance).type}`);
         }
         return result;
     }
@@ -31,18 +35,17 @@ function statement (invoice, plays) {
         { style: "currency", currency: "USD",
             minimumFractionDigits: 2 }).format;
 
-    for (let perf of invoice.performances) {
-        const play = plays[perf.playID];
 
-        const thisAmount = amountFor(play, perf);
+    for (let perf of invoice.performances) {
+        const thisAmount = amountFor(perf);
 
         // add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0);
         // add extra credit for every ten comedy attendees
-        if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
 
         // print line for this order
-        result += `  ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
+        result += `  ${playFor(perf).name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
         totalAmount += thisAmount;
     }
     result += `Amount owed is ${format(totalAmount/100)}\n`;
