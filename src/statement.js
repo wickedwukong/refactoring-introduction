a1349ef5483484c37d813322fe2a86541faa4371
Xuemin Guan <Xuemin Guan>
2019-11-10 00:05:28 +0000

    splitting phase by extracting renderPlainText and adding a data object

--- src/statement.js
+++ src/statement.js
@@ -1,6 +1,6 @@
 export {statement}
 
-function statement (invoice, plays) {
+function renderPlainText(data, invoice, plays) {
     let result = `Statement for ${invoice.customer}\n`;
     for (let perf of invoice.performances) {
         result += `  ${playFor(perf).name}: ${(usd(amountFor(perf)))} (${perf.audience} seats)\n`;
@@ -67,4 +67,9 @@ function statement (invoice, plays) {
         }
         return totalAmount;
     }
+}
+
+function statement (invoice, plays) {
+    const statementData = {}
+    return renderPlainText(statementData, invoice, plays);
 }
