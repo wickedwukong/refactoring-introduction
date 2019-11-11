7261b3178d21794d222d44ae1383cfd6c2546551
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-11-11 16:54:40 +0000

    Move amount() to subclasses ragedyPerformanceCalculator and ComedyPerformanceCalculator

--- src/createStatementData.js
+++ src/createStatementData.js
@@ -5,25 +5,7 @@ class PerformanceCalculator {
     }
 
     get amount() {
-        let result = 0;
-        switch (this.performance.play.type) {
-            case "tragedy":
-                result = 40000;
-                if (this.performance.audience > 30) {
-                    result += 1000 * (this.performance.audience - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if (this.performance.audience > 20) {
-                    result += 10000 + 500 * (this.performance.audience - 20);
-                }
-                result += 300 * this.performance.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${this.performance.play.type}`);
-        }
-        return result;
+        throw new Error('subclass responsibility');
     }
 
     get volumeCredits() {
@@ -36,9 +18,25 @@ class PerformanceCalculator {
 }
 
 class TragedyPerformanceCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 40000;
+        if (this.performance.audience > 30) {
+            result += 1000 * (this.performance.audience - 30);
+        }
+        return result
+    }
 }
 
 class ComedyPerformanceCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 30000;
+        if (this.performance.audience > 20) {
+            result += 10000 + 500 * (this.performance.audience - 20);
+
+        }
+        result += 300 * this.performance.audience;
+        return result;
+    }
 }
 
 function createPerformanceCalculator(result, play) {
