41b1a690bae70386b20b95b00e4f740dcf53383b
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-11-12 14:43:40 +0000

    Extract totalAmount. Split loop, Extract function, Slide statement, and inline variable

--- src/statement.js
+++ src/statement.js
@@ -1,19 +1,22 @@
 export {statement}
 
 function statement(invoice, plays) {
-    let totalAmount = 0;
     let result = `Statement for ${invoice.customer}\n`;
-
-
     for (let perf of invoice.performances) {
-        // print line for this order
         result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
-        totalAmount += amountFor(perf);
     }
 
-    result += `Amount owed is ${usd(totalAmount)}\n`;
+    result += `Amount owed is ${usd(totalAmount())}\n`;
     result += `You earned ${(totalVolumeCredits())} credits\n`;
     return result;
+
+    function totalAmount() {
+        let totalAmount = 0;
+        for (let perf of invoice.performances) {
+            totalAmount += amountFor(perf);
+        }
+        return totalAmount;
+    }
 
     function usd(value) {
         return new Intl.NumberFormat("en-US",
