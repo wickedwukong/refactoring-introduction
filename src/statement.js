d3194a755be3c91c63b50a990694b4556d8d31a2
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-11-13 11:11:31 +0000

    Replace temp with query to remove the need to pass in play

--- src/statement.js
+++ src/statement.js
@@ -46,10 +46,10 @@ function statement (invoice, plays) {
         { style: "currency", currency: "USD",
             minimumFractionDigits: 2 }).format;
 
-    function amountFor(aPlay, aPerformance) {
+    function amountFor(aPerformance) {
         let result = 0;
 
-        switch (aPlay.type) {
+        switch (playFor(aPerformance).type) {
             case "tragedy":
                 result = 40000;
                 if (aPerformance.audience > 30) {
@@ -64,22 +64,25 @@ function statement (invoice, plays) {
                 result += 300 * aPerformance.audience;
                 break;
             default:
-                throw new Error(`unknown type: ${play.type}`);
+                throw new Error(`unknown type: ${playFor(aPerformance).type}`);
         }
         return result;
     }
 
+    function playFor(perf) {
+        return plays[perf.playID];
+    }
+
     for (let perf of invoice.performances) {
-        const play = plays[perf.playID];
-        let thisAmount = amountFor(play, perf);
+        let thisAmount = amountFor(perf);
 
         // add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0);
         // add extra credit for every ten comedy attendees
-        if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
 
         // print line for this order
-        result += `  ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
+        result += `  ${playFor(perf).name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
         totalAmount += thisAmount;
     }
     result += `Amount owed is ${format(totalAmount/100)}\n`;
