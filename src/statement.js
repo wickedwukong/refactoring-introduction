e7ec585512f4f05b76812c3a6040533712a4820c
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-11-13 15:56:13 +0000

    adding data

--- src/statement.js
+++ src/statement.js
@@ -1,5 +1,44 @@
 export {statement}
 
+// plays.json…
+//
+//   {
+//       "hamlet": {"name": "Hamlet", "type": "tragedy"},
+//       "as-like": {"name": "As You Like It", "type": "comedy"},
+//       "othello": {"name": "Othello", "type": "tragedy"}
+//   }
+
+ // invoices.json…
+
+  // // [
+  //     {
+  //         "customer": "BigCo",
+  //         "performances": [
+  //             {
+  //                 "playID": "hamlet",
+  //                 "audience": 55
+  //             },
+  //             {
+  //                 "playID": "as-like",
+  //                 "audience": 35
+  //             },
+  //             {
+  //                 "playID": "othello",
+  //                 "audience": 40
+  //             }
+  //         ]
+  //     }
+  // ]
+
+// output
+
+// Statement for BigCo
+//     Hamlet: $650.00 (55 seats)
+// As You Like It: $580.00 (35 seats)
+// Othello: $500.00 (40 seats)
+// Amount owed is $1,730.00
+// You earned 47 credits
+
 function statement (invoice, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
