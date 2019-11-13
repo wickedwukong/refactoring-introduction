1abc74cca55743f2c788d42429263591cb1eace1
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-11-13 16:23:43 +0000

    extract amountFor

--- src/statement.js
+++ src/statement.js
@@ -49,6 +49,20 @@ function statement (invoice, plays) {
 
     for (let perf of invoice.performances) {
         const play = plays[perf.playID];
+        // add volume credits
+        volumeCredits += Math.max(perf.audience - 30, 0);
+        // add extra credit for every ten comedy attendees
+        if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+
+        // print line for this order
+        result += `  ${play.name}: ${format(amountFor(play, perf)/100)} (${perf.audience} seats)\n`;
+        totalAmount += amountFor(play, perf);
+    }
+    result += `Amount owed is ${format(totalAmount/100)}\n`;
+    result += `You earned ${volumeCredits} credits\n`;
+    return result;
+
+    function amountFor(play, perf) {
         let thisAmount = 0;
 
         switch (play.type) {
@@ -68,17 +82,8 @@ function statement (invoice, plays) {
             default:
                 throw new Error(`unknown type: ${play.type}`);
         }
-
-        // add volume credits
-        volumeCredits += Math.max(perf.audience - 30, 0);
-        // add extra credit for every ten comedy attendees
-        if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
-
-        // print line for this order
-        result += `  ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
-        totalAmount += thisAmount;
+        return thisAmount;
     }
-    result += `Amount owed is ${format(totalAmount/100)}\n`;
-    result += `You earned ${volumeCredits} credits\n`;
-    return result;
+
+
 }
