ed92bfa6f893e2cb578ee481e16be1da983a6a2a
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-11-13 09:43:45 +0000

    Extract amountFor

--- src/statement.js
+++ src/statement.js
@@ -1,5 +1,43 @@
 export {statement}
 
+// plays.json
+//
+//   {
+//       "hamlet": {"name": "Hamlet", "type": "tragedy"},
+//       "as-like": {"name": "As You Like It", "type": "comedy"},
+//       "othello": {"name": "Othello", "type": "tragedy"}
+//   }
+
+// invoices.json
+// [
+//     {
+//         "customer": "BigCo",
+//         "performances": [
+//             {
+//                 "playID": "hamlet",
+//                 "audience": 55
+//             },
+//             {
+//                 "playID": "as-like",
+//                 "audience": 35
+//             },
+//             {
+//                 "playID": "othello",
+//                 "audience": 40
+//             }
+//         ]
+//     }
+// ]
+
+// output:
+//
+// Statement for BigCo
+//     Hamlet: $650.00 (55 seats)
+// As You Like It: $580.00 (35 seats)
+// Othello: $500.00 (40 seats)
+// Amount owed is $1,730.00
+// You earned 47 credits
+
 function statement (invoice, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
@@ -8,27 +46,32 @@ function statement (invoice, plays) {
         { style: "currency", currency: "USD",
             minimumFractionDigits: 2 }).format;
 
-    for (let perf of invoice.performances) {
-        const play = plays[perf.playID];
-        let thisAmount = 0;
+    function amountFor(aPlay, aPerformance) {
+        let result = 0;
 
-        switch (play.type) {
+        switch (aPlay.type) {
             case "tragedy":
-                thisAmount = 40000;
-                if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
+                result = 40000;
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
                 }
                 break;
             case "comedy":
-                thisAmount = 30000;
-                if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
+                result = 30000;
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20);
                 }
-                thisAmount += 300 * perf.audience;
+                result += 300 * aPerformance.audience;
                 break;
             default:
                 throw new Error(`unknown type: ${play.type}`);
         }
+        return result;
+    }
+
+    for (let perf of invoice.performances) {
+        const play = plays[perf.playID];
+        let thisAmount = amountFor(play, perf);
 
         // add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0);
