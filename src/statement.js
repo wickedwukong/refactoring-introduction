9b6d85ca33e734ec782b92600210426940e02aba
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-11-13 12:29:06 +0000

    Split loop and extract totalAmount()

--- src/statement.js
+++ src/statement.js
@@ -39,17 +39,23 @@ export {statement}
 // You earned 47 credits
 
 function statement(invoice, plays) {
-    let totalAmount = 0;
     let result = `Statement for ${invoice.customer}\n`;
-
+    
     for (let perf of invoice.performances) {
         result += `  ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
-        totalAmount += amountFor(perf);
     }
 
-    result += `Amount owed is ${usd(totalAmount / 100)}\n`;
+    result += `Amount owed is ${usd(totalAmount() / 100)}\n`;
     result += `You earned ${(totalVolumeCredits())} credits\n`;
     return result;
+
+    function totalAmount() {
+        let result = 0;
+        for (let perf of invoice.performances) {
+            result += amountFor(perf);
+        }
+        return result;
+    }
 
     function totalVolumeCredits() {
         let result = 0;
