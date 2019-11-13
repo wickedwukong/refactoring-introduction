b192abe80f237bfe3b9e9b5f428d65e1f4d5ffd3
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-11-13 15:04:42 +0000

    Add htmlStatement()

--- src/statement.js
+++ src/statement.js
@@ -40,6 +40,14 @@ export {statement}
 // Amount owed is $1,730.00
 // You earned 47 credits
 
+function statement(invoice, plays) {
+    return renderPlainText(createStatementData(invoice, plays));
+}
+
+function htmlStatement (invoice, plays) {
+    return renderHtml(createStatementData(invoice, plays));
+}
+
 function renderPlainText(statementData) {
     let result = `Statement for ${statementData.customer}\n`;
 
@@ -51,15 +59,26 @@ function renderPlainText(statementData) {
     result += `You earned ${(statementData.totalVolumeCredits)} credits\n`;
     return result;
 
-    function usd(value) {
-        return new Intl.NumberFormat("en-US",
-            {
-                style: "currency", currency: "USD",
-                minimumFractionDigits: 2
-            }).format(value);
-    }
 }
 
-function statement(invoice, plays) {
-    return renderPlainText(createStatementData(invoice, plays));
+function usd(value) {
+    return new Intl.NumberFormat("en-US",
+        {
+            style: "currency", currency: "USD",
+            minimumFractionDigits: 2
+        }).format(value);
+}
+
+function renderHtml (data) {
+    let result = `<h1>Statement for ${data.customer}</h1>\n`;
+    result += "<table>\n";
+    result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
+    for (let perf of data.performances) {
+        result += `  <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+        result += `<td>${usd(perf.amount)}</td></tr>\n`;
+    }
+    result += "</table>\n";
+    result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
+    return result;
 }
